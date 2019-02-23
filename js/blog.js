5b3dd6bfc6c7c0a2856454c89c6e366c161fe41b
RonLek <rohanlekhwani@ymail.com>
2019-02-24 01:06:31 +0530

    Added World Domination! and Updated Resume

--- js/blog.js
+++ js/blog.js
@@ -21,7 +21,7 @@ $(function () {
 				var srcStart = srcIndex + 5; // Find where the actual image URL starts; 5 for the length of 'src="'
 				var srcEnd = item.description.substring(srcStart).indexOf('"') + srcStart; // Find where the URL ends
 				var src = item.description.substring(srcStart, srcEnd); // Extract just the URL
-				output += '<div class="blog-element"><img class="img-responsive" src="' + src + '" width="360px" height="240px"></div></header>';
+				output += '<div class="blog-element"><img class="img-responsive" src="' + src + '" width="100%" height="300"></div></header>';
 				output += '<div class="blog-content"><h4><a href="'+ item.link + '">' + item.title + '</a></h4>';
 				output += '<div class="post-meta"><span>By ' + item.author + '</span></div>';
 				var yourString = item.description.replace(/<img[^>]*>/g,""); //replace with your string.
